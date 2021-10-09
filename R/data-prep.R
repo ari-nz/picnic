40867e05ffcf658615f7f6811e205503874904df
Ari Angelo <hello@ari.nz>
2021-10-09 02:43:38 +0000

    Add FS structure

---
+++ R/data-prep.R
@@ -0,0 +1,29 @@
+
+
+dir_data = 'data'
+fs_parks = file.path(dir_data, 'parks.rds')
+fs_alcho = file.path(dir_data, 'alchohol.rds')
+
+
+
+
+
+
+
+if(!file.exists(fs_parks)){
+    parks = sf::st_read(file.path(dir_data, 'parks.geojson'))
+    parks = rmapshaper::ms_simplify(parks,keep=0.2)
+    saveRDS(parks, fs_parks)
+}
+if(!file.exists(fs_alcho)){
+    alcho = sf::st_read(file.path(dir_data, 'alchohol.geojson'))
+    alcho = rmapshaper::ms_simplify(alcho,keep=0.2)
+    saveRDS(alcho, fs_alcho)
+}
+
+
+cat(as.character(Sys.time()),' - loading alcho zones\n')
+alcho = readRDS(fs_alcho)
+cat(as.character(Sys.time()),' - loading parks\n')
+parks = readRDS(fs_parks)
+parks = parks %>% dplyr::filter(ParkExtentSHAPE_Area < quantile(parks$ParkExtentSHAPE_Area,c(0.999)))
---
+++ R/obj-clicked_points.R
@@ -0,0 +1,124 @@
+library(R6)
+library(dplyr)
+
+
+Points = R6::R6Class(
+    classname = 'demopoints',
+
+    private=list(
+        #For shiny reactive deps
+        reactiveDep = NULL,
+        reactiveExpr = NULL,
+        invalidate = function() {
+            private$count <- private$count + 1
+            private$reactiveDep(private$count)
+            cat(private$count)
+            invisible()
+        },
+        count = 0,
+
+        #store clicked points and status
+        clicked_points = tibble::tibble(
+            lat =numeric(),
+            lng = numeric(),
+            active = logical(),
+            pointId = integer()
+
+        )
+
+    ),
+
+
+    public = list(
+
+        initialize = function() {
+            private$reactiveDep <- function(x) NULL
+
+        },
+
+        reactive = function() {
+            # Ensure the reactive stuff is initialized.
+            if (is.null(private$reactiveExpr)) {
+                private$reactiveDep <- reactiveVal(0)
+                private$reactiveExpr <- reactive({
+                    private$reactiveDep()
+                    self
+                })
+            }
+            private$reactiveExpr
+        },
+
+        #Add points to df on click
+        add = function(x) {
+
+            private$clicked_points <- tibble::add_row(
+                private$clicked_points,
+                lat = x$lat,
+                lng = x$lng,
+                active=TRUE,
+                pointId = nrow(private$clicked_points)+1
+                )
+
+            private$invalidate()
+
+            invisible(self)
+        } ,
+        remove = function(id){
+            private$clicked_points = dplyr::rows_update(
+                private$clicked_points,
+                tibble(pointId=id,active=FALSE)
+                )
+            private$invalidate()
+            invisible(self)
+
+        },
+
+        #Print methods
+        print = function() {
+            cat("<Points>\n", sep = "")
+            private$clicked_points
+        },
+        get_active = function(){
+            # cat("Active <Points>\n", sep = "")
+            s=private$clicked_points
+            if(nrow(s)>0){
+                s=s %>%
+                    dplyr::filter(active)
+            }
+
+            s
+        }
+
+
+
+    )
+
+)
+
+
+
+#Reactive Shiny
+if(FALSE){
+
+
+
+cp1 = list(lat = -36.8900555751941, lng = 174.754028320313, .nonce = 0.878921042598185)
+p =Points$new()
+p$add(cp1)
+pr <- p$reactive()
+# The observer accesses the reactive expression
+o <- observe({
+    message("Point added:")
+})
+shiny:::flushReact()
+
+#> Person changed. Name: Dean
+
+# Note that this is in isolate only because we're running at the console; in
+# typical Shiny code, the isolate() wouldn't be necessary.
+isolate(pr()$add(cp1))
+shiny:::flushReact()
+}
+
+
+
